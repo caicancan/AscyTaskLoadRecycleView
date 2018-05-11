e815f5ccd882b157bd0ede4c3f8929ece3c5ab09
caicancan <18062411472@163.com>
2018-05-11 16:19:58 +0800

    添加aidl的构造，将本服务器端的aidl目录整个拷贝到需要调用的sever端进行调用，放在main目录下

---
+++ app/src/main/java/com/ffpy/demo/HelloService.java
@@ -0,0 +1,31 @@
+package com.ffpy.demo;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+
+public class HelloService extends Service {
+    IMyAidlInterface.Stub mStub = new IMyAidlInterface.Stub() {
+        //aidl文件里面提供的方法
+        @Override
+        public int add(int arg1, int arg2) throws RemoteException {
+            //具体是怎么实现的
+                         return arg1 + arg2;
+                    }
+
+        @Override
+        public void show() throws RemoteException {
+            Log.i("ccc","执行服务端的aidl");
+        }
+
+
+    };
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return mStub;
+    }
+}
